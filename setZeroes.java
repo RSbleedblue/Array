e2e84d2e5e0fca0caf2cd7110786b0acaa194b50
Rivansh Srivastava <48222792+RSbleedblue@users.noreply.github.com>
2023-09-13 15:18:33 +0530

    Create setZeroes.java

---
+++ setZeroes.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.* ;
+
+public class Solution {
+    public static void setZeros(int matrix[][]) {
+ 
+        boolean row[] = new boolean[matrix.length];
+        boolean col[] = new boolean[matrix[0].length];
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(matrix[i][j]==0){
+                    row[i] = true;
+                    col[j] = true;
+                }
+            }
+        }
+        for(int i=0;i<matrix.length;i++){
+            if(row[i]){
+                for(int j=0;j<matrix[0].length;j++){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        for(int j=0;j<matrix[0].length;j++){
+            if(col[j]){
+                for(int i=0;i<matrix.length;i++){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
+}
